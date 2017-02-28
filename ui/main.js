c974d403aece35b2a2263e5841e63bd89d67c5f3
rahuliet17 <rockerahul97@gmail.com>
2017-03-01 02:18:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,18 @@ button.onclick = function() {
     request.onreadystatechange = function(){
         if(request.readyState===XMLHttpRequest.DONE)
         if(request.status===200){
-            var counter= request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML= counter.toString();
+            var names=['Name1','Name2','Name3'];
+            var list='';
+            for (var i=0;i< names.length; i++){
+            list+='<li>'+names[i]+'</li>';
+            }
+            var ul= document.getElementById('namelist');
+            ul.innerHTML=list;
         }
     };
-request.open('GET','http://rahuliet17.imad.hasura-app.io/counter',true);
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+request.open('GET','http://rahuliet17.imad.hasura-app.io/submit-name?'+name,true);
 request.send(null);
 };
 var nameInput = document.getElementById('name');
@@ -18,11 +24,4 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
-    var names=['Name1','Name2','Name3'];
-    var list='';
-    for (var i=0;i< names.length; i++){
-    list+='<li>'+names[i]+'</li>';
-    }
-    var ul= document.getElementById('namelist');
-    ul.innerHTML=list;
 };
