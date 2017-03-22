abb4cc1ce003e7f26687c90baa14bfb87e006715
rahuliet17 <rockerahul97@gmail.com>
2017-03-23 00:14:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'rahuliet17'
+    database: 'rahuliet17'
+    host: 'db.imad.hasura-app.io',
+    port: '5432'
+    password:process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -73,10 +82,22 @@ function createTemplate(data){
     `;
     return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var pool= new Pool(config);
+app.get('/test-db',function(req,res){
+   //make a select request  
+    //return response with the result
+    pool.query('SELECT * FROM test', function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           res.send(JSON.stringfy(result));
+       }
+    });
+});
 var counter = 0;
 app.get('/counter', function(req, res){
     counter=counter+1;
