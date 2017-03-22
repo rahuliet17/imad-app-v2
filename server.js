eb4288268f050fe9caba19be20dfb7595468582d
rahuliet17 <rockerahul97@gmail.com>
2017-03-23 00:56:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'rahuliet17',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'db-rahuliet17-16599'
+    password: db-rahuliet17-16599
 };
 
 var app = express();
